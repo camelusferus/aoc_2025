b39ad612fccdc3fbaab4c367b0b42faf874acaed
camelusferus <nils@trampel.org>
2025-12-01 06:18:40 +0100

    AoC 2024 Day 1

---
+++ aoc_2025_01_1.py
@@ -0,0 +1,15 @@
+with open("inputs/input_01.txt") as f:
+    input = [(a[0], int(a[1:])) for a in f.read().strip('\n').split("\n")]
+
+state = 50
+times_0 = 0
+
+for element in input:
+    if element[0] == 'L':
+        state -= element[1]
+    else:
+        state += element[1]
+    if state % 100 == 0:
+        times_0 += 1
+
+print(times_0)

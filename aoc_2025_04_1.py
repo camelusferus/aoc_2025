c0010f3d3f485a9ddbbe5ce5ae226afa08df66a3
camelusferus <nils@trampel.org>
2025-12-04 11:01:27 +0100

    AoC 2025 Day 4

---
+++ aoc_2025_04_1.py
@@ -0,0 +1,22 @@
+with open("inputs/input_04.txt") as file:
+    grid = [[x for x in a] for a in file.read().strip("\n").split("\n")]
+
+accessible = 0
+
+for y in range(len(grid)):
+    for x in range(len(grid[0])):
+        adj = 0
+        for x_neigh in [-1, 0, 1]:
+            for y_neigh in [-1, 0, 1]:
+                if ((x_neigh or y_neigh)
+                        and x + x_neigh <= len(grid[0])-1
+                        and y + y_neigh <= len(grid)-1
+                        and x + x_neigh >= 0
+                        and y + y_neigh >= 0):
+                    if grid[y + y_neigh][x + x_neigh] == '@':
+                        adj += 1
+
+        if adj < 4 and grid[y][x] == '@':
+            accessible += 1
+
+print(accessible)
